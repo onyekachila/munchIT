66db0a6601f20ac6c1c602b9901a5cacb68b186d
Onyekachi Stanley <onyekachila@gmail.com>
2019-03-27 14:32:10 +0100

    Refactored the controller to use a service to get menus grouped by the category name where restaurant id is matching the criteria

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Menu;
+use App\Services\MenuService;
 
 class HomeController extends Controller
 {
@@ -22,9 +22,12 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(MenuService $service)
     {
-        $menus = Menu::all();
-        return view('home', compact('menus'));
+        $resto_ids = [1];
+
+        $categories = $service->getMenuWithCategory($resto_ids);
+
+        return view('home', compact('categories'));
     }
 }
